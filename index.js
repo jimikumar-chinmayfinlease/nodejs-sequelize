ebdd33b4cb8ebca7d25b9ad8b0232bd0339c79cc
jimikumar-chinmayfinlease <jimikumar.chhatrala@chinmayfinlease.com>
2024-09-11 17:28:03 +0530

    Model Defination using NodeJS, MySQL, and Sequelize ORM

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-require('./models')
+const User = require('./models/user');
 
 app.use(bodyParser.json());
 
 app.get("/", function (req,res){
     res.send('Hello World');
 });
+
+// User.sync();
+User.sync({force: true });
+// User.sync({alter: true });
+// User.drop();
 
 app.listen(3006, () => {
     console.log('App will run on: http://localhost:3006');
--- models/index.js
+++ models/index.js
@@ -11,3 +11,5 @@ try {
 } catch (error) {
     console.error('Unable to connect to the database: ', error);
 }
+
+module.exports = sequelize;
